9e3e4f0611789bf4f23b57046c1b2a726d5f9f12
jo860612 <john860612@gmail.com>
2018-01-04 01:20:59 +0800

    Checkout

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('Restaurant', () => App);
+import item from './Checkout';
+AppRegistry.registerComponent('Restaurant', () =>item);
